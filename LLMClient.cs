f7e4a8366c34a2cb2e02ecb768dc6464df4db0d1
Nick <ko.binghong@gmail.com>
2026-01-27 08:56:16 +0800

    Read some SDK examples and put them into README
    - client.CreateResponse could access previous resopnses via their id strings, can modify code accordingly
    - naive design is to create an OpenAIMessage record which inherits from Message

--- LLMClient.cs
+++ LLMClient.cs
@@ -1,6 +1,8 @@
 using States;
 using OpenAI;
 using OpenAI.Responses;
+using System.Text.Json;
+using Microsoft.VisualBasic;
 
 namespace LLM {
     /// <summary>
@@ -15,26 +17,56 @@ namespace LLM {
 
     // "wrapper classes" of the sdk provided classes (if they are provided)
     public class OpenAILLMClient : ILLMClient {
-        // we are talking to the response endpoint: https://platform.openai.com/docs/api-reference/responses/create
-#pragma warning disable OPENAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-
+        // we are talking to the response endpoint:
+        // https://platform.openai.com/docs/api-reference/responses/create
         private readonly ResponsesClient client = new(
-#pragma warning restore OPENAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-
-
             model: "gpt-5-nano-2025-08-07`",
-
             apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY")
         );
-        Task<string> makeRequest(List<Message> state) {
-            // call the endpoint
-            // 1. Serialize `state`, and determine how much should be passed as parameter
-            // 2. Deserialize the response, and only return content[0].text
+
+        public async Task<string> makeRequest(List<Message> state) {
+            // convert state to a parameter acceptable by CreateResponse and
+            // determine how much should be passed as parameter
+            
+			var prompt = state[state.Count - 1];
+            var context = state[state.Count - 2];   // this in general shall be a slice (subarray) of state
+
+            // 2. call the end point.
+            var response = await client.CreateResponse(
+                userInputText: prompt.Content,
+                previousResponseId: // some Response id,
+            );
+
+            // 3. Deserialize the response, and only return content[0].text
+            // 4. Handle possible error on failed calls
+            
+            
+
         }
     }
     public class ClaudeLLMClient : ILLMClient {
+        public ClaudeLLMClient() {
+            throw new NotImplementedException();
+        }
+        public async Task<string> makeRequest(List<Message> state) {
+            throw new NotImplementedException(); 
+        }
     }
     public class AzureLLMClient : ILLMClient {
+        public AzureLLMClient() {
+            throw new NotImplementedException();
+        }
+        public async Task<string> makeRequest(List<Message> state) {
+            throw new NotImplementedException(); 
+        }
+    }
+
+    // helper class dedicated to serialize data using C# generics
+    public class Serializer {
+        static JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+        public static string Serialize<T>(T value) {
+            return JsonSerializer.Serialize(value, options: options);
+        }
     }
 
 }
--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # C# Console App
 * Console app built with C# for the sake of learning. 
-* Current objective is to build a Mininal "LLM-Shell" application.
-* The goal is to evovle this into an AI agent desktop app.
+* Current objective is to build a Mininal "LLM-Shell" desktop application.
 
 a minimal data-flow diagram ![Data Flow Diagram](App_Data_Flow.png)
+
+# Learnt Concepts
+1. **Vibe Coding**:
++ How to leverage the power of chatGPT to learn a new programming language and build a usable app.
+2. **OO programming**: 
++ How to model the entire app as interactions between entities with distinct roles.
+3. **Pattern matching**:
++ `switch` expression
++ `=>` (arrow function) for lambda functions.
+4. **Adaptor pattern**:
++ Use `interface` (or **abstract base class** in C++) as wrapper around a service(dependency), so the service could easily interact with the rest of the program through the client.
++ The client is implemented based on the contract defined in the `interface`/**abstract base class**.
++ Adaptors implement the client and wraps around the service.
 
 # Resources
 ## Csharp Tutorial
 1.  https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/
 ## OpenAI API
 1.  https://platform.openai.com/docs/api-reference/authentication?utm_source=chatgpt.com
-2.  https://www.nuget.org/packages/OpenAI#how-to-use-responses-with-streaming-and-reasoning
+2.  https://github.com/openai/openai-dotnet/tree/main
+3.  https://platform.openai.com/docs/guides/text
+4. 
 
